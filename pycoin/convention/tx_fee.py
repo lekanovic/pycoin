a4bf39e4c2c7d3815834b305467ff960798938cb
Radovan Lekanovic <lekanovic@gmail.com>
2015-03-28 23:11:43 -0700

    Check if we have dust output. Then we need to pay fee.
    Refactor the code.

--- pycoin/convention/tx_fee.py
+++ pycoin/convention/tx_fee.py
@@ -1,8 +1,7 @@
 import io
-from pycoin.tx.Tx import Tx
 
 TX_FEE_PER_THOUSAND_BYTES = 10000
-
+TRESH_HOLD = 57600000
 
 def recommended_fee_for_tx(tx):
     """
@@ -16,24 +15,26 @@ def recommended_fee_for_tx(tx):
       - update for bitcoind v0.90 new fee schedule
     """
     TRESH_HOLD = 57600000
+    s = io.BytesIO()
+    tx.stream(s)
+
     in_tx = sum(1 for t in tx.txs_in)
     out_tx = sum(1 for t in tx.txs_out)
 
+    # Are all outputs 0.01 BTC or larger.(Dust)
+    no_dust = all(t.coin_value >= 1000000 for t in tx.txs_out)
+
     size_bytes = ((in_tx*148) + (out_tx*32) + 10)
-    priority = 0
-    for t in tx.unspents:
-      priority += t.coin_value * t.confirmations
 
-    value = priority / size_bytes
+    priority = (sum(t.coin_value * t.confirmations for t in tx.unspents)) / size_bytes
 
-    s = io.BytesIO()
-    tx.stream(s)
     tx_byte_count = len(s.getvalue())
-    tx_fee = TX_FEE_PER_THOUSAND_BYTES * ((999+tx_byte_count)//1000)
 
     # No fee are needed if we are above the treshold and
     # the transaction is less then 1000 bytes.
-    if value > TRESH_HOLD and tx_byte_count < 1000:
+    if priority > TRESH_HOLD and tx_byte_count < 1000 and no_dust:
       return 0
+
+    tx_fee = TX_FEE_PER_THOUSAND_BYTES * ((999+tx_byte_count)//1000)
 
     return tx_fee
